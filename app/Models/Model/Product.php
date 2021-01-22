1621b7f7e461f008e82bdafbf295b0422b0e1453
omidiora <omidioraemmanuel@gmail.com>
2021-01-22 18:07:13 +0100

    Proudct Update

--- app/Models/Model/Product.php
+++ app/Models/Model/Product.php
@@ -10,6 +10,11 @@ class Product extends Model
 {
     use HasFactory;
 
+
+protected $fillable=[
+    "name" , "detail" , "stock" , "price" ,"discount"
+];
+
 public function reviews(){
 
     return $this->hasMany(Review::class);
